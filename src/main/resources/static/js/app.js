b6019e9477a38973eeab34d39abf90e4a439a01a
Matthew Hug <matthewhug@Matthews-Air.cfl.rr.com>
2017-04-12 13:46:50 -0400

    Still working on twitch connect

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,6 +4,9 @@ angular.module("app", [])
 
         $(document).ready(function(){
             console.log("Welcome to Streamers!");
+            Twitch.init({clientId: 'q1xf84du1sgbb7a5ryqn3un7epxzw5'}, function(error, status) {
+                // the sdk is now loaded
+            });
             Twitch.getStatus(function(err,status){
                 if(status.authenticated) {
                     console.log("Authenticated");
