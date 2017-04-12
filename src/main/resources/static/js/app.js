dd11c6932e91b5fcb695f46a99e649d26856ccaa
Matthew Hug <matthewhug@Matthews-Air.cfl.rr.com>
2017-04-12 12:34:38 -0400

    Working on twitch connect

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,10 +2,20 @@ angular.module("app", [])
 
     .controller('login', function($scope, $http) {
 
+        $(document).ready(function(){
+            console.log("Welcome to Streamers!");
+            Twitch.getStatus(function(err,status){
+                if(status.authenticated) {
+                    console.log("Authenticated");
+                }
+            });
+        });
+
         $scope.twitchLogin = function() {
             Twitch.login({
                 scope: ['user_read', 'channel_read']
             });
+            alert(Twitch.getToken());
         };
 
     })
